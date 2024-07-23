aa10a4999de535d42b3e55e8ebf93830c1fffff0
Bityacht-Developer <55692615+dennislee928@users.noreply.github.com>
2024-07-24 05:08:38 +0800

    add:ci/cd yaml

--- app.vue
+++ app.vue
@@ -18,6 +18,9 @@
         {{ period }}
       </div>
     </div>
+    <div class="confirm">
+      <button class="button-27" role="button">confirm changes</button>
+    </div>
   </div>
 </template>
 
@@ -100,6 +103,9 @@ const getClass = (day, period) => {
   padding: 0px;
   border-color: crimson;
 }
+.confirm {
+  size: 25ch;
+}
 @keyframes shiny {
   0% {
     background-position: 0% 50%;
@@ -107,5 +113,49 @@ const getClass = (day, period) => {
   100% {
     background-position: 100% 50%;
   }
+}
+
+/* CSS */
+.button-27 {
+  appearance: none;
+  background-color: #d71ad7;
+  border: 2px solid #1a1a1a;
+  border-radius: 15px;
+  box-sizing: border-box;
+  color: #ffffff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica,
+    Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  font-size: 16px;
+  font-weight: 600;
+  line-height: normal;
+  margin: 0;
+  min-height: 60px;
+  min-width: 0;
+  outline: none;
+  padding: 16px 24px;
+  text-align: center;
+  text-decoration: none;
+  transition: all 300ms cubic-bezier(0.23, 1, 0.32, 1);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 100%;
+  will-change: transform;
+}
+
+.button-27:disabled {
+  pointer-events: none;
+}
+
+.button-27:hover {
+  box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
+  transform: translateY(-2px);
+}
+
+.button-27:active {
+  box-shadow: none;
+  transform: translateY(0);
 }
 </style>
